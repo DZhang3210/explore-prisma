db0382a3f636818f4b63623f6114b0e68ce7458e
DZhang3210 <53838139+DZhang3210@users.noreply.github.com>
2024-07-29 15:58:17 -0400

    new commit

--- actions/actions.ts
+++ actions/actions.ts
@@ -4,19 +4,24 @@ import prisma from "@/lib/db"
 import { revalidatePath } from "next/cache"
 
 export async function createPost(formData: FormData) {
-    await prisma.post.create({
-        data: {
-            title: formData.get("title") as string,
-            slug: (formData.get('title') as string)
-                .replace(/\s+/g, "-")
-                .toLowerCase(),
-            content: formData.get('content') as string,
-            author: {
-                connect: {
-                    email: 'davidzhang3210@gmail.com'
+    try {
+        await prisma.post.create({
+            data: {
+                title: formData.get("title") as string,
+                slug: (formData.get('title') as string)
+                    .replace(/\s+/g, "-")
+                    .toLowerCase(),
+                content: formData.get('content') as string,
+                author: {
+                    connect: {
+                        email: 'davidzhang3210@gmail.com'
+                    }
                 }
             }
-        }
-    })
-    revalidatePath('/')
+        })
+        revalidatePath('/')
+    }
+    catch (error) {
+
+    }
 }
